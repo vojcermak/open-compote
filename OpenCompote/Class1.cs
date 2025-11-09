70ba15aa6c4a5afce917fc68ca53cff46d55cfdc
Vojtěch Čermák <vojcermak@seznam.cz>
2025-11-09 13:24:53 +0100

    Project init files

---
+++ OpenCompote/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace OpenCompote;
+
+public class Class1
+{
+    public static void Print(string text)
+    {
+        Console.WriteLine(text);
+    }
+}
---
+++ testConsole/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenCompote;
+
+Console.WriteLine("Hello, World!");
+
+
+Class1.Print("This is my library");
