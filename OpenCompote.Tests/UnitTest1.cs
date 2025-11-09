5d3e183429035a7983cfa6016baf28b931dff23d
Vojtěch Čermák <vojcermak@seznam.cz>
2025-11-09 14:38:45 +0100

    Added test project, renamed projects to have structured names

---
+++ OpenCompote.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿namespace OpenCompote.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var result = 5 + 5;
+        Assert.Equal(10, result);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = 5 + 5;
+        Assert.Equal(10, result);
+    }
+
+}
