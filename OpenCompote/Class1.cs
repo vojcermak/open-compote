827aed3d9870e88d266b63d5040410faa1837265
Vojtěch Čermák <vojcermak@seznam.cz>
2025-11-14 15:08:44 +0100

    Added test comment and fix gitignore

--- OpenCompote/Class1.cs
+++ OpenCompote/Class1.cs
@@ -2,6 +2,10 @@
 
 public class Class1
 {
+    /// <summary>
+    /// Document generation test
+    /// </summary>
+    /// <param name="text">TEST PARAM</param>
     public static void Print(string text)
     {
         Console.WriteLine(text);
